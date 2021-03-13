75a188836fdcc7e84ad9875f0c97f4120a9cda6d
LubJur <75535869+LubJur@users.noreply.github.com>
2021-03-13 18:03:56 +0100

    fixed the bug and added an optional parameter -a

--- PiHole_EInk_Graph.py
+++ PiHole_EInk_Graph.py
@@ -3,34 +3,46 @@ import time
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 import pihole as ph
+import argparse
 
-#Write your PiHole IP between the quotation marks:
-pihole = ph.PiHole("")
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", type=str, help="IP address of a system running PiHole", default="127.0.0.1")
+piholeip = parser.parse_args()
 
-#Write your PiHole Password between the quotation marks:
-pihole.authenticate("")
-
-pihole.refresh()
+try:
+    pihole = ph.PiHole(piholeip.a)
+    pihole.refresh()
+except OSError:
+    print("This IP does not have a PiHole running")
+    quit()
 
 epd = epd2in13_V2.EPD()
 epd.init(epd.FULL_UPDATE)
 
-def make_graph():
+x_keys = 0
+y_values = 0
+
+def get_data():
+    global x_keys
+    global y_values
     domains = pihole.getGraphData()["domains"]  # options: domains or ads
     x_keys = domains.keys()
     x_keys = [*x_keys]
     y_values = domains.values()
     y_values = [*y_values]
+    del(domains)
+    return x_keys, y_values
 
+def make_graph(x_keys, y_values):
     plt.figure(figsize=(1.911417, 0.9334646), dpi = 126, frameon = False)  # display size in inches (width, height) and DPI
     plt.tick_params(axis = "y", which = "both", right = False, width = 0.5, labelsize = 6)
     plt.tick_params(axis = "x", which = "both", top = False, bottom = False, labelbottom = False)
     plt.box(False)
     plt.title("Domains", size = "x-small")
-    plt.plot(x_keys, y_values, color = "k", linewidth = 0.5)
-
+    plt.plot(x_keys, y_values, color = "k", linewidth = 0.005)
     plt.savefig('line_plot.png', transparent = True, bbox_inches = "tight", pad_inches = 0)  
-
+    plt.close("all")
+    plt.clf()
     img = Image.open("line_plot.png")
     img = img.convert('1')   
     img = img.resize((250, 122))  # display resolution (width, height), used when creating the .bmp
@@ -42,14 +54,23 @@ def show():
     image1.paste(bmp, (0, 0))
     epd.display(epd.getbuffer(image1))
 
+get_data()
+first_values = y_values
+
 try:
+    make_graph(x_keys, y_values)
+    show()
     while True:
-        print("Refreshed")
-        make_graph()
-        show()
-        time.sleep(300)  # 900s = 15 min
+        get_data()
+        if first_values != y_values:
+            make_graph(x_keys, y_values)
+            show()
+            first_values = y_values
+        del(y_values)
+        del(x_keys)
+        time.sleep(300)  # 300s = 5 min
 except KeyboardInterrupt:
-    print("KeyboardInterrupt")
+    print("\nKeyboardInterrupt, clearing display")
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
     epd.sleep()
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ PiHole graph on a Waveshare 2.13" E-Ink display
 * PiHole-api 2.6
 
 ## Dependencies
-* Raspberry Pi (tested and developed on Raspberry Pi Zero WH, should work on others too)
+* Raspberry Pi 
 * [Waveshare 2.13" E-Ink display V2](https://www.waveshare.com/2.13inch-e-paper-hat.htm)
     * For displays with other dimension you need to change sizes on lines 25 and 36
 * [Waveshare e-Paper libraries](https://github.com/waveshare/e-Paper)
@@ -26,14 +26,14 @@ PiHole graph on a Waveshare 2.13" E-Ink display
 
 ## Setup
 1. Install RaspberryPi OS (preferably the Lite version without desktop) on a RaspberryPi connected to the E-Ink display
-2. Install [PiHole](https://pi-hole.net/) using command `curl -sSL https://install.pi-hole.net | bash`
+2. Install [PiHole](https://pi-hole.net/) using command `$ curl -sSL https://install.pi-hole.net | bash`
     * for additional options, check PiHole [documentation](https://github.com/pi-hole/pi-hole/#one-step-automated-install)
 3. install the E-Ink display according to the instruction on [Waveshare wiki](https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT)
 4. Write down the **IP address** and **password** for the admin interface
-5. Install matplotlib with `sudo apt-get install python3-matplotlib`
-6. Install PiHole-api with `pip3 install PiHole-api`
-7. Open *PiHole_EInk_Graph.py* and write your PiHole **IP address** between the quotation marks on line **8** and your **password** on line **11**
-8. Run *PiHole_EInk_Graph.py* with `python3 PiHole_EInk_Graph.py`
-    * note: PiHole must have the graph already displayed on the web UI for it to show on the display
+5. Install matplotlib with `$ sudo apt-get install python3-matplotlib`
+6. Install PiHole-api with `$ pip3 install PiHole-api`
+7. Run *PiHole_EInk_Graph.py* with `$ nohup python3 PiHole_EInk_Graph.py &`
+    * **Optional:** You can add an IP that is different from the PiHole running locally by adding a parameter `-a` and an ip (for example: `$ nohup python3 PiHole_EInk_Graph.py -a 192.168.255.255 &`)
+    * note: PiHole must have some graph data already for it to display
     * Raspberry Pi might reboot at first start, after the reboot just start the program again
-9. If you want to exit the program you can press **Ctrl + C** which clears the E-Ink display and closes the application
+8. Passing a KeyboardInterrupt will clear the display and then exit
